c3ff7df7f2dfff4a8674df1bcc0c1752e41cbef1
Brian Lister <brian@lister.design>
2024-03-12 15:42:23 -0500

    feat: create transport

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import build from 'pino-abstract-transport';
+import { WebhookClient, EmbedBuilder, type ColorResolvable } from 'discord.js';
+
+const embedColors: Record<string, ColorResolvable> = {
+	10: '#c100ff', //trace
+	20: '#c100ff', //debug
+	30: '#8be836', //info
+	40: '#ffc142', //warn
+	50: '#e83938', //error
+	60: '#e83938', //fatal
+};
+
+async function discordTransport(options: { webhookURL: string }) {
+	if (!options.webhookURL) {
+		throw new Error('The required option: webhookURL is missing.');
+	}
+
+	const webhook = new WebhookClient({
+		url: options.webhookURL,
+	});
+
+	return build(async function (source) {
+		for await (const message of source) {
+			const embed = new EmbedBuilder()
+				.setColor(embedColors[message.level]!)
+				.setTitle(String(message.msg))
+				.setDescription(
+					message.err.stack
+						? String(message.err.stack).slice(0, 4000)
+						: 'No stack trace',
+				)
+				.setTimestamp();
+
+			await webhook.send({
+				embeds: [embed],
+			});
+		}
+	});
+}
+
+export default discordTransport;
